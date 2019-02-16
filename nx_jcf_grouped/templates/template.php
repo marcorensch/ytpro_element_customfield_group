a6f21c18896bb441323dad4602c8e41c236bbf88
Marco <marco.rensch@area59.ch>
2019-02-16 12:35:36 +0100

    Version 1.0

--- nx_jcf_grouped/templates/template.php
+++ nx_jcf_grouped/templates/template.php
@@ -1,16 +1,21 @@
 <?php
 
 function shortener($string, $n){
+    $x = $n-1;
     $arr = explode(' ',trim($string));
-    if(count($arr) > 1){
-        $new_string = '<span class="uk-hidden@m">'.$arr[$n].'</span>';
+    if(count($arr) > 1 && count($arr) >= $x){
+        $new_string = '<span class="uk-hidden@m">'.$arr[$x].'</span>';
         $new_string .= '<span class="uk-visible@m">'.$string.'</span>';
     }else{
         $new_string = '<span class="">'.$string.'</span>';
     }
     
     return $new_string;
-}
+};
+
+function templater($string, $templatestring){
+    return $html;
+};
 
 $el = $this->el('div', [
 
@@ -24,32 +29,48 @@ $i = 1;
 $child_count = count($children);
 
 ?>
-<div class="uk-child-width-auto" uk-grid>
+<div class="uk-child-width-auto uk-flex uk-flex-<?= $props['container_alignement']?>" uk-grid>
 <?php
-foreach($children as $child){
-    if(!empty($child->props['fieldname'])){
-        if(array_key_exists($child->props['fieldname'], $cfields)){
-            echo '<div class="">' . $cfields[$child->props['fieldname']]['value'] . '</div>';
-        }else{
-            echo '<div> console.log("nx-Joomla! Customfield Plugin for Yootheme Pro: Customfield '.$child['fieldname'].' not found in this article");</div>';
-        };
-    }else{
-        echo '<div> console.log("nx-Joomla! Customfield Plugin for Yootheme Pro: no Customfield configured");</div>';
-    };
-    // Divider
-    if(is_numeric($props['divider_interval'])){
-        if($i%$props['divider_interval'] === 0){
-            if($i+1 <= $child_count){
-                echo '<div class="">' . $props['divider'] . '</div>';
+if(count($children) > 0){
+    foreach($children as $child){
+        if(!empty($child->props['fieldname']) && !in_array($child->props['fieldname'], [null, 'name-of-field'])){
+            if(array_key_exists($child->props['fieldname'], $cfields)){
+                // Shortener
+                if($child->props['useshort']){
+                    $string = shortener($cfields[$child->props['fieldname']]['value'], $child->props['short_to']);
+                }else{
+                    $string = $cfields[$child->props['fieldname']]['value'];
+                };
+                // Templater
+                if(!empty($child->props['template']) && strpos('%value%', $child->props['template'])){
+                    $tmpl_string = templater($string, $child->props['template']);
+                }else{
+                    if(intval($props['debug'])) echo '<script> console.log("nx-Joomla! Customfield Plugin for Yootheme Pro: Customfield Template not setted up");</script>';
+                    $tmpl_string = $string;
+                };
+                echo '<div class="">' . $tmpl_string . '</div>';
+            }else{
+                if(intval($props['debug'])) echo '<script> console.log("nx-Joomla! Customfield Plugin for Yootheme Pro: Customfield '.$child->props['fieldname'].' not found in this article");</script>';
             };
+        }else{
+            if(intval($props['debug'])) echo '<script> console.log("nx-Joomla! Customfield Plugin for Yootheme Pro: no Customfield configured");</script>';
         };
-    }elseif(empty($props['divider_interval'])){
-        // not set so do nothing
-    }else{
-        echo '<script> console.log("nx-Joomla! Customfield Plugin for Yootheme Pro: '.$props['divider_interval'].' is not a numeric value as interval!");</script>';
-    }
-    $i++;
+        // Divider
+        if(is_numeric($props['divider_interval'])){
+            if($i%$props['divider_interval'] === 0){
+                if($i+1 <= $child_count){
+                    echo '<div class="">' . $props['divider'] . '</div>';
+                };
+            };
+        }elseif(empty($props['divider_interval'])){
+            // not set so do nothing
+        }else{
+            if(intval($props['debug'])) echo '<script> console.log("nx-Joomla! Customfield Plugin for Yootheme Pro: '.$props['divider_interval'].' is not a numeric value as interval!");</script>';
+        }
+        $i++;
+    };
 };
+
 ?>
 </div>
 <?= $el($props, $attrs) ?>
